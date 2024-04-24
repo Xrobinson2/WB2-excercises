187e02b48b4be583da8356b2638f6a834eb03f73
Xavier Robinson <x.robinson5041@outlook.com>
2024-04-24 11:05:45 -0400

    added conversions

---
+++ NumericConversions/conversion_test.js
@@ -0,0 +1,62 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+"use strict"
+
+//define the variables
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+
+// parseInt 
+
+ a = parseInt(" 101.1 ");
+var b = parseInt("55");
+var c = parseInt("402 Stevens");
+var d = parseInt("Number 5 ");
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
+// parseFloat 
+
+var a = parseFloat(" 101.1 ");
+var b = parseFloat("55");
+var c = parseFloat("402 Stevens");
+var d = parseFloat("Number 5 ");
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+// Number 
+
+var a = Number(" 101.1 ");
+var b = Number("55");
+var c = Number("402 Stevens");
+var d = Number("Number 5 ");
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+//Unary 
+
+var a = +" 101.1 ";
+var b = +"55";
+var c = +"402 Stevens";
+var d = +"Number 5 ";
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
